9f333bbe6a1ddc76596cade1ddc4cccea17bd41d
Gurdeep <123107592+gurdeeep@users.noreply.github.com>
2023-09-01 00:03:20 +0530

    Create guureyy.py

---
+++ guureyy.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+def find_largest(a, b, c):
+    return max(a, b, c)
+
+def main():
+    st.title("Find the Largest Number")
+
+    st.write("Enter three numbers:")
+    a = st.number_input("Number 1")
+    b = st.number_input("Number 2")
+    c = st.number_input("Number 3")
+
+    if st.button("Find Largest"):
+        largest = find_largest(a, b, c)
+        st.write(f"The largest number is: {largest}")
+main()
